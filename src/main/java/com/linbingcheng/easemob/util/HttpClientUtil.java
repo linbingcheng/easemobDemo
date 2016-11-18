a41953737f55ef7c2210d86420a8736f5c3c65c3
master <bingchenglin@qq.com>
2016-11-18 17:45:08 +0800

    修改不必要信息

---
+++ src/main/java/com/linbingcheng/easemob/util/HttpClientUtil.java
@@ -0,0 +1,8 @@
+package com.linbingcheng.easemob.util;
+
+/**
+ * Created by bingchenglin on 2016/11/18.
+ */
+public class HttpClientUtil {
+
+}
---
+++ src/main/java/com/linbingcheng/easemob/util/JerseyUtil.java
@@ -0,0 +1,7 @@
+package com.linbingcheng.easemob.util;
+
+/**
+ * Created by bingchenglin on 2016/11/18.
+ */
+public class JerseyUtil {
+}
