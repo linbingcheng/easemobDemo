687c29f0df075bb4225d3eb5296d253109996b56
linbingchneg <bingchenglin@qq.com>
2016-11-20 17:49:31 +0800

    HTTPGET封装

---
+++ src/main/java/com/linbingcheng/easemob/util/Contents.java
@@ -0,0 +1,20 @@
+package com.linbingcheng.easemob.util;
+
+/**
+ * Created by linbingchneg on 2016/11/20.
+ */
+public interface Contents {
+
+    class HttpProtocol{
+        public static final String DEFAULT_HTTP_CHATSET = "UTF-8";
+        public static final int CONNECTION_TIMEOUT = 5000;
+        public static final int SOCKET_TIMEOUT = 5000;
+        public static final int CONNECTION_REQUEST_TIME = 1000;
+        public static final String APPLICATION_JSON = "application/json";
+        public static final String APPLICATION_XML = "application/xml";
+        public static final String TEXT_PLAIN="text/plain";
+
+    }
+
+
+}
