2f1965724e8dae5f99839d7ee0e8578a58bfd49f
master <bingchenglin@qq.com>
2016-11-21 20:28:13 +0800

    update

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import com.linbingcheng.easemob.api.AuthTokenAPI;
+import com.linbingcheng.easemob.common.ClientContext;
+import com.linbingcheng.easemob.common.EasemobRestAPIFactory;
+
+/**
+ * Created by bingchenglin on 2016/11/21.
+ */
+public class Main {
+    public static void main(String[] args) {
+        EasemobRestAPIFactory factory = ClientContext.getInstance().init(ClientContext.INIT_FROM_PROPERTIES).getAPIFactory();
+        ClientContext context = factory.getContext();
+        AuthTokenAPI authToken = (AuthTokenAPI) factory.newInstance(EasemobRestAPIFactory.TOKEN_CLASS);
+        String token = String.valueOf(authToken.getAuthToken(context.getClientId(), context.getClientSecret()));
+        System.out.println("++++");
+        System.out.println(token);
+        System.out.println("+++");
+    }
+}
