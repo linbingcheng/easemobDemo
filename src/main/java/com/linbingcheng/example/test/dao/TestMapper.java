4ee0081d9d3a4b29eb26ab59da64c79eac292027
master <bingchenglin@qq.com>
2016-12-12 15:34:03 +0800

    update

--- src/main/java/com/linbingcheng/example/test/dao/TestMapper.java
+++ src/main/java/com/linbingcheng/example/test/dao/TestMapper.java
@@ -1,15 +1,29 @@
 package com.linbingcheng.example.test.dao;
 
 import com.linbingcheng.example.test.model.Test;
+import com.linbingcheng.example.test.model.TestExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface TestMapper {
+    long countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(Test record);
 
     int insertSelective(Test record);
 
+    List<Test> selectByExample(TestExample example);
+
     Test selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
 
     int updateByPrimaryKeySelective(Test record);
 
